d2972630c84ae2f6aa3119985ef3ff4312faaf36
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-01-19 03:55:44 +0000

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -8,9 +8,23 @@ class AttractionsController < ApplicationController
         @attraction = Attraction.new(attraction_params)
         if @attraction.save
             redirect_to attraction_path(@attraction)
-     
-            
         end
+    end 
+    
+    def edit 
+        # binding.pry
+        @attraction = Attraction.find(params[:id])
+    end 
+    
+    def update
+        # binding.pry
+        @attraction = Attraction.find(params[:id])
+        @attraction.update(name: params[:attraction][:name]) 
+        @attraction.update(min_height: params[:attraction][:min_height]) 
+        @attraction.update(happiness_rating: params[:attraction][:happiness_rating]) 
+        @attraction.update(nausea_rating: params[:attraction][:nausea_rating]) 
+        @attraction.update(tickets: params[:attraction][:tickets]) 
+        redirect_to attraction_path(@attraction)
     end 
     
     def index
