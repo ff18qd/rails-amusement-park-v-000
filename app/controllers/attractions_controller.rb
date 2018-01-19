cf6577f5b723c2323e2db1c77b3816e10540ea19
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-01-19 03:38:46 +0000

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,7 +1,21 @@
 class AttractionsController < ApplicationController
     
+    def new 
+        @attraction = Attraction.new
+    end 
+    
+    def create
+        @attraction = Attraction.new(attraction_params)
+        if @attraction.save
+            redirect_to attraction_path(@attraction)
+     
+            
+        end
+    end 
+    
     def index
         @attractions = Attraction.all
+        @user = User.find(session[:user_id])
     end 
     
     def show
@@ -9,4 +23,12 @@ class AttractionsController < ApplicationController
 
         @attraction = Attraction.find(params[:id])
     end 
+    
+     
+    private 
+    
+    def attraction_params
+      params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
+    end
+    
 end 
